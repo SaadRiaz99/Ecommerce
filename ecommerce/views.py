3cb408b0ce4f1a752f53907dcc87c2fa7fe59145
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-03-02 15:39:34 +0500

    Add Contact url

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -5,6 +5,8 @@ def home(request):
     return render(request  , 'home.html')
 def shop(request):
     return render(request  , 'shop.html')
+def contact(request):
+    return render(request  , 'contact.html')
 
 
 
