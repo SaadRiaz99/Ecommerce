fbded77839237d656b1f7d1017eeccb2d0793d46
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-03-02 15:36:21 +0500

    Add Css File shop and also contact

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render , HttpResponse , redirect
 
 def home(request):
     return render(request  , 'home.html')
+def shop(request):
+    return render(request  , 'shop.html')
 
 
 
