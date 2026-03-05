7f6da7445c79e823f821691029661a608a474c66
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-03-05 15:54:03 +0500

    Add Model and Contact page beckend

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -2,3 +2,18 @@ from django.db import models
 from . import views
 
 # Create your models here.
+# class authtencate(models.Model):
+#     username = Models.charField(max_length = 50 , null = False)
+#     Password = Models.charField(max_length = 10 , null = False)
+
+class Contact(models.Model):
+    name = models.CharField(max_length=50 , null=False),
+    email = models.EmailField(max_length=120 , null=False)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f"{self.name} - {self.email}"
+
+
